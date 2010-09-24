9f8e323af144ebe04cab987c1b122fd120a84806
Alexis Sukrieh <sukria@sukria.net>
2010-09-24 11:08:39 +0200

    no more load_plugin

--- lib/jitterbug/WebService.pm
+++ lib/jitterbug/WebService.pm
@@ -1,9 +1,7 @@
 package jitterbug::WebService;
 
-BEGIN {
-    use Dancer ':syntax';
-    load_plugin 'jitterbug::Plugin::Redis';
-}
+use Dancer ':syntax';
+use jitterbug::Plugin::Redis;
 
 use File::Spec;
 
