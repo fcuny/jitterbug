26df149efd3b962dafa3ccd9e6c1f1f54734477c
Jonathan "Duke" Leto <jonathan@leto.net>
2011-01-13 18:10:12 -0500

    Put commit message in subject of failure emails and improve body format

--- lib/jitterbug/Emailer.pm
+++ lib/jitterbug/Emailer.pm
@@ -23,14 +23,20 @@ sub run {
     my $project    = $task->project->name;
     my $tap_output = $self->{'tap_output'};
     my $sha1       = $task->commit->sha256;
+    my $shortsha1  = substr($sha1, 0, 8);
     my $desc       = JSON::decode_json( $task->commit->content );
     my $email      = $desc->{'author'}{'email'};
+    my $message    = $desc->{'message'};
     my $header     = $buildconf->{'on_failure_header'};
     my $footer     = $buildconf->{'on_failure_footer'};
 
     my $body = <<BODY;
 $header
 
+Commit Message:
+$message
+
+TAP Output:
 $tap_output
 
 $footer
@@ -42,7 +48,7 @@ BODY
                 ->cc($buildconf->{'on_failure_cc_email'})
                 ->text_body($body)
                 ->subject(
-                    $buildconf->{'on_failure_subject_prefix'} . "$project @ $sha1"
+                    $buildconf->{'on_failure_subject_prefix'} . "$project @ $shortsha1 $message"
                   );
                 # Should we attach a build log for convenience?
                 # ->attach(io('dead_bunbun_faked.gif')->all,
