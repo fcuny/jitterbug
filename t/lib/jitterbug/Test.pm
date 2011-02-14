bf1368bb3f1c4d6f8be9a492d4ceddbee2330977
franck cuny <franck@lumberjaph.net>
2011-02-14 21:46:20 +0100

    fix script name

--- t/lib/jitterbug/Test.pm
+++ t/lib/jitterbug/Test.pm
@@ -4,7 +4,7 @@ use warnings;
 use FindBin qw($Bin);
 
 BEGIN{
-   qx{$^X -Ilib $Bin/../scripts/deploy_schema $Bin/data/test.yml}
+   qx{$^X -Ilib $Bin/../scripts/jitterbug_db -c $Bin/data/test.yml --deploy}
       unless -r qq{$Bin/data/jitterbug.db};
 };
 
