ec18b6e5b6d99cdf0566af2805092ad5bdda9085
franck cuny <franck@lumberjaph.net>
2011-05-09 09:38:31 +0200

    show more informations about a task (closes GH #55)
    Signed-off-by: franck cuny <franck@lumberjaph.net>

--- lib/jitterbug/Task.pm
+++ lib/jitterbug/Task.pm
@@ -8,12 +8,19 @@ get '/:task_id' => sub {
     my $task_id = params->{task_id};
 
     my $task = schema->resultset('Task')->find($task_id);
+    my $commit =
+      schema->resultset('Commit')->find( { sha256 => $task->sha256 } );
 
     if (!$task) {
         send_error("task does not exist!", 404);
     }
 
-    template 'task/index', {task => $task };
+    if (!$commit){
+        render_error("commit doesn't exists", 404);
+    }
+
+    my $content = from_json($commit->content);
+    template 'task/index', {task => $task, commit => $content };
 };
 
 1;
